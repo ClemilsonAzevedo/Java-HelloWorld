24d9ce6d54ec8b19749d667a2e681095a04c1ff2
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-10-04 04:59:22 +0100

    finish hello world and sum Numbers

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+import br.com.clemilson.model.calcVal;
+
+public class HelloWorld {
+  
+  public static void main(String[] args) {
+
+    calcVal calcVal = new calcVal();
+
+    int result = calcVal.sumTwoNumbers(40, 20);
+    System.out.println(result);
+  }
+}
---
+++ src/br/com/clemilson/model/calcVal.java
@@ -0,0 +1,7 @@
+package br.com.clemilson.model;
+
+public class calcVal {
+    public int sumTwoNumbers(int number1, int number2){
+    return number1 + number2;
+  }
+}
